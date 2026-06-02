1919dc178dc6bda3bb6ae078beea0c7b08996b0d
Tonio <schricke_anthony@yahoo.fr>
2026-06-02 16:57:57 +0200

    [ASC] code factorisation

--- src/main.cpp
+++ src/main.cpp
@@ -19,40 +19,32 @@ static int current_time = 0;
 static int last_time = 0;
 static double fps = 0.0; //Le nb de fps
 
-void initLight(void);
-void idle(void);
-void Reshape(int w, int h);
-void render(void);
-void keyboard(unsigned char c, int x, int y);
-void mouse(int button, int state, int x, int y);
+void initLight(void) {
+  GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+  GLfloat mat_shininess[] = { 50.0 };
+  GLfloat light_position[] = { 4.0, 4.0, 4.0, 0.0 };
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glShadeModel (GL_SMOOTH);
 
-int main(int argc, char** argv) {
+  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-  ObjParser * parser = new ObjParser();
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+}
 
-  angle = -1.0;
-  //float valZoom = 0.0;
-
+void init3D(int argc, char** argv) {
   // Pour un écran Widescreen ratio d'aspect = 16:9
+  //[ASC] penser a une struct ?
   long width = 800;
   long height = 450;
   float zNear = 0.1;
   float zFar = 50.0;
   float zoomFactor = 1.0;
 
-
-  if (argv[1] != NULL) {
-    printf("Lecture du fichier... \t %s\n", argv[1]);
-    objets = parser->readFile(argv[1]);
-  } else {
-    printf("Veuillez passer en argument le nom du fichier obj (exemple: ./essai3D ./obj/jaguard.obj\n");
-    return 0;
-  }
-
-
-  parser->~ObjParser();
-
-  glutInit(&argc, argv);
+  glutInit(&argc, argv); // https://www.opengl.org/resources/libraries/glut/spec3/node10.html
   glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 
   glutInitWindowPosition(100, 100);
@@ -74,42 +66,9 @@ int main(int argc, char** argv) {
 
   initLight();
   gluLookAt(0.0, 6.0, 6.0, 0.0, 0.0, 0.0, 0.0 , 1.0, 0.0);
-
-  //Initialisation des objets (chargement dans la carte graphique pour le rendu
-  for(it = objets.begin(); it != objets.end(); it++) {
-    it->init();
-  }
-
-  glutDisplayFunc(render);
-  glutIdleFunc(idle);
-  //glutReshapeFunc(Reshape);
-  glutKeyboardFunc(keyboard);
-  glutMouseFunc(mouse);
-
-  glutMainLoop();
-}
-
-
-void initLight(void)
-{
-  GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-  GLfloat mat_shininess[] = { 50.0 };
-  GLfloat light_position[] = { 4.0, 4.0, 4.0, 0.0 };
-  glClearColor (0.0, 0.0, 0.0, 0.0);
-  glShadeModel (GL_SMOOTH);
-
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-
 }
 
 void idle(void) {
-
   frame++;
   current_time = glutGet(GLUT_ELAPSED_TIME);
 
@@ -124,21 +83,7 @@ void idle(void) {
   glutSwapBuffers();
 }
 
-void render(void) {
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glRotatef(angle,0.0,1.0,0.0);
-
-  for(it = objets.begin(); it != objets.end(); it++) {
-    it->dessiner();
-  }
-
-  glutSwapBuffers();
-  glFlush();
-}
-
-void Reshape(int w, int h)
-{
+void Reshape(int w, int h) {
   glViewport( 0, 0, (GLint)w, (GLint)h );
   glMatrixMode( GL_PROJECTION );
   glLoadIdentity();
@@ -147,19 +92,58 @@ void Reshape(int w, int h)
   glLoadIdentity();
 }
 
+void render(void) {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glRotatef(angle,0.0,1.0,0.0);
+
+  for(it = objets.begin(); it != objets.end(); it++)
+    it->dessiner();
+
+  glutSwapBuffers();
+  glFlush();
+}
+
 void keyboard(unsigned char c, int x, int y){
   // ASCH - 29/09/2014 - La touche 27 est la touche echap
-  if (c == 27) {
+  if (c == 27)
     exit(0);
-  }
-
 }
 
 void mouse(int button, int state, int x, int y){
-  if(button == GLUT_RIGHT_BUTTON) {
+  if(button == GLUT_RIGHT_BUTTON)
     exit(0);
-  }
 }
 
+void load3DFunc() {
+  glutDisplayFunc(render);
+  glutIdleFunc(idle);
+  //glutReshapeFunc(Reshape);
+  glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouse);
+}
 
+int main(int argc, char** argv) {
+  ObjParser * parser = new ObjParser();
+  angle = -1.0;
 
+  std::vector<string> obj_files;
+  obj_files.push_back("obj/jaguard.obj");
+  obj_files.push_back("obj/maison.obj");
+  //obj_files.push_back("rail.obj");
+  //obj_files.push_back("The_rocket_locomotive_1800_pour_integration.obj");
+
+  for(std::vector<string>::iterator files = obj_files.begin(); files!=obj_files.end(); files++) {
+    printf("Lecture du fichier... \t %s\n", (*files).c_str());
+    objets = parser->readFile((*files).c_str());
+  }
+
+  parser->~ObjParser();
+  init3D(argc, argv);
+
+  //Initialisation des objets (chargement dans la carte graphique pour le rendu
+  for(it = objets.begin(); it != objets.end(); it++)
+    it->init();
+
+  load3DFunc();
+  glutMainLoop(); //[ASC] est-ce le chargement de la fonction ??
+}
